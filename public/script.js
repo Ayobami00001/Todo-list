fbfc118269d3455817e6c11295721f885ae8884e
Ayobami00001 <ibrahimfaruqayobami@gmail.com>
2025-08-09 15:07:38 +0100

    delete and edit

--- public/script.js
+++ public/script.js
@@ -70,12 +70,50 @@ var editindex;
             
         }
     }
+    function editItems(i) {
+        editindex = i
+    }
+    function editItem(i) {
+    editindex = i
+}
+    function editIte(i) {
+    editindex = i
+}
+    function editIt(i) {
+    editindex = i           
+}
+function editgangan() {
+    allitems.splice(editindex,1,newItems.value)
+    displayItems()
+    showToast('Item edited successfully','top','center','green','black')
+}
+function editganga() {
+   allitem.splice(editindex,1,newItem.value)
+    displayItem()
+    showToast('Item edited successfully','top','center','green','black')
+}
+function editgang() {
+    allite.splice(editindex,1,newIte.value)
+    displayIte()
+    showToast('Item edited successfully','top','center','green','black')
+}
+function editgan() {
+    allit.splice(editindex,1,newIt.value)
+    displayIt()
+    showToast('Item edited successfully','top','center','green','black')
+}
+ 
+
+
+
+
     function displayItems() {
         showitems.innerHTML = ''
           allitems.map((data,index)=>{
               showitems.innerHTML += 
                 `<h5 class="text-dark mt-2 ms-3" >${index+1}.${data}</h5>
-                
+                <button onclick="deleteItems(${index})" class="btn btn-danger btn-sm">Delete</button>
+            <button onclick="editItems(${index})" data-bs-toggle="modal" data-bs-target="#exampleModal" class="btn btn-warning btn-sm">Edit</button>
                 `
             })
     }
@@ -84,7 +122,8 @@ var editindex;
           allitem.map((data,index)=>{
               showitem.innerHTML += 
                 `<h5 class="text-dark mt-2 ms-3" >${index+1}.${data}</h5>
-                
+                <button onclick="deleteItem(${index})" class="btn btn-danger btn-sm">Delete</button>
+            <button onclick="editItem(${index})" data-bs-toggle="modal" data-bs-target="#exampleModal2" class="btn btn-warning btn-sm">Edit</button>
                 `
             })
     }function displayIte() {
@@ -92,7 +131,8 @@ var editindex;
           allite.map((data,index)=>{
               showite.innerHTML += 
                 `<h5 class="text-dark mt-2 ms-3" >${index+1}.${data}</h5>
-                
+                <button onclick="deleteIte(${index})" class="btn btn-danger btn-sm">Delete</button>
+            <button onclick="editIte(${index})" data-bs-toggle="modal" data-bs-target="#exampleModal3" class="btn btn-warning btn-sm">Edit</button>
                 `
             })
     }function displayIt() {
@@ -100,6 +140,56 @@ var editindex;
           allit.map((data,index)=>{
               showit.innerHTML += 
               `
-              <h5 class="text-dark mt-2 ms-3 " style="overflow-wrap: break-word;" >${index+1}.${data}</h5>`
+              <h5 class="text-dark mt-2 ms-3 " style="overflow-wrap: break-word;" >${index+1}.${data}</h5>
+              <button onclick="deleteIt(${index})" class="btn btn-danger btn-sm">Delete</button>
+            <button onclick="editIt(${index})" data-bs-toggle="modal" data-bs-target="#exampleModal4" class="btn btn-warning btn-sm">Edit</button>
+              `
           })
         }
+        function deleteItems(index) {
+    var sure = confirm("Are you sure you want to delete this!");
+    if (sure) {
+        allitems.splice(index, 1);
+        showToast('Item deleted successfully', 'top', 'center', 'red', 'black');
+        displayItems();
+    }
+    else {
+        showToast('Deletion cancelled', 'top', 'center', 'blue', 'black');
+    }
+}
+    function deleteItem(index) {
+        var sure = confirm("Are you sure you want to delete this!")
+        if (sure) {
+        allitem.splice(index,1);
+        showToast('Item deleted successfully','top','center','red','black');
+        displayItem();
+        }else {
+            showToast('Deletion cancelled','top','center','blue','black');
+        }
+        
+    }
+    function deleteIte(index) {
+        var sure = confirm("Are you sure you want to delete this!")
+        if (sure) { 
+        allite.splice(index,1)
+        showToast('Item deleted successfully','top','center','red','black');
+        displayIte();
+        }else {
+            showToast('Deletion cancelled','top','center','blue','black');
+        }
+        
+    }
+    function deleteIt(index) {
+        var sure = confirm("Are you sure you want to delete this!")
+    if (sure) {
+        allit.splice(index,1)
+        showToast('Item deleted successfully','top','center','red','black');
+        displayIt();
+        }else {
+            showToast('Deletion cancelled','top','center','blue','black');
+        }
+        
+    }
+
+
+        
